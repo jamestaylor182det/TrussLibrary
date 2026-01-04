4e5cb60fadbf079ec0492634f27592b1e9eff152
jamestaylor182det <63136820+jamestaylor182det@users.noreply.github.com>
2026-01-04 16:44:16 +1100

    Add files via upload

---
+++ testSketchV2.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(120);
+  text("Testing", width / 2, height / 2);
+  text("Testing 123", width / 2 + 20, height / 2);
+}
